c0c7dbf732d753f43b0b5809dcbc85c812fd38c8
erikazanetti <erikazanetti.25@gmail.com>
2026-06-01 11:06:13 +0200

    Create 0.m

---
+++ 0.m
@@ -0,0 +1,44 @@
+clear
+clc
+close all
+
+%% Read dataset
+
+T = readtable("Ebro_dataset_MATLAB.xlsx", ...
+    "Sheet", "Sentinel-RAdCor data", ...
+    "VariableNamingRule", "preserve");
+
+%% Define input features and target
+
+bandCols = {'B1_mean','B2_mean','B3_mean','B4_mean','B5_mean','B6_mean','B7_mean','B8_mean','B8a_mean'};
+
+targetCol = 'TSS_mean';
+
+X = table2array(T(:, bandCols));
+Y = T.(targetCol);
+
+%% Basic checks
+
+disp("Size of T:")
+disp(size(T))
+
+disp("Size of X:")
+disp(size(X))
+
+disp("Size of Y:")
+disp(size(Y))
+
+disp("Missing values in X:")
+disp(sum(isnan(X), "all"))
+
+disp("Missing values in Y:")
+disp(sum(isnan(Y), "all"))
+
+disp("TSS minimum:")
+disp(min(Y))
+
+disp("TSS maximum:")
+disp(max(Y))
+
+disp("TSS mean:")
+disp(mean(Y))
